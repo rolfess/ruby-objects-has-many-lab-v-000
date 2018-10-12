1724050312c0eb6c01bff211033da6f2ec646253
Susan Rolfes <rolfes57069@yahoo.com>
2018-10-12 21:44:19 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,14 @@
 class Artist
   attr_accessor :song, :name
 
-  def initialize (name)
+  def initialize (name, song)
     @name = name
-    #@songs = song
+    @songs = song
     @songs = []
   end
 
   def add_song_by_name (song)
-    @songs = song
+    #@songs = song
     @songs << song
     song.artist = self
   end
